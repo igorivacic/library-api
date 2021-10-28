cd89445dc94ef44b2d31b75bf042c10a9733a1e8
Igor Ivacic <igor.ivacic.2740@gmail.com>
2021-10-28 15:00:24 +0200

    Mount LibraryApp API.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   use_doorkeeper
   devise_for :users
+
+  mount LibraryApp::API => '/api'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
